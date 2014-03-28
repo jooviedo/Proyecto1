59af5672be72672b9e7bf97f2746e7d2633f94b9
johan <johanop8027@gmail.com>
2014-03-27 23:04:24 -0600

    primer commmit hola mundo

---
+++ main.c
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.c
+ * Author: arturo
+ *
+ * Created on 27 de marzo de 2014, 05:43 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("hola mundo!");
+
+    return (EXIT_SUCCESS);
+}
+
